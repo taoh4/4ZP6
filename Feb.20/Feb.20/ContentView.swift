87b8b688e03fd15b65769bf74322e4ab395fa11e
Haoyang <51974410+taoh4@users.noreply.github.com>
2022-02-20 13:06:50 -0500

    initial commit

---
+++ Feb.20/Feb.20/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Feb.20
+//
+//  Created by SpicaE on 2022/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Feb.20/Feb.20/Feb_20App.swift
@@ -0,0 +1,17 @@
+//
+//  Feb_20App.swift
+//  Feb.20
+//
+//  Created by SpicaE on 2022/2/20.
+//
+
+import SwiftUI
+
+@main
+struct Feb_20App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
