42d27945d4e98d2c968fd35a7679a8ea86945b3d
kanh3 <kanh3@mcmaster.ca>
2022-04-05 21:32:54 -0400

    touch log

---
+++ Feb.26/myproject/pages/multiple-touch/multiple-touch.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import {
+    Animated,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Image,
+    button_strong,
+    TouchableWithoutFeedback,
+    ImageBackground,
+    TouchableOpacity,
+} from 'react-native';
+import { Dimensions } from 'react-native';
+
+const TouchView = ({navigation}) => {
+    const [countL, setCountL] = useState(0);
+    const [countR, setCountR] = useState(0);
+
+    return (
+        <View
+            style={{flex:1, flexDirection: 'column', justifyContent: 'space-between'}}
+        >
+            <ImageBackground
+                source={require("../../assets/garden.jpeg")}
+                style={[styles.image]}
+            >
+            </ImageBackground>
+            <View style = {{flex:1}}>
+                <Text style = {[styles.text]}>Left: {countL}, Right: {countR}</Text>
+            </View>
+            <View style = {{flexDirection: 'row',  flex:1, justifyContent: 'space-between'}}>
+                <TouchableOpacity
+                    style={[styles.button1]}
+                    onPress={() => {setCountL(countL+1), console.log("Left pressed")}}
+                >
+                    <Text style={[styles.text, styles.center]}>Tap</Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    style={[styles.button2]}
+                    onPress={() => {setCountR(countR+1), console.log("Right clicked")}}
+                >
+                    <Text style={[styles.text, styles.center]}>Tap</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    backgound: {
+        flex: 1,
+        height: '100%',
+    },
+
+    image: {
+        height: '100%',
+        width: '100%',
+    },
+
+    center: {
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    text: {
+        color: "#FAF9F9",
+        padding: 15,
+        fontSize: 20,
+        textAlign: 'center'
+    },
+
+    button1: {
+        flex: 1,
+        backgroundColor: '#B8D5B8',
+        borderRadius: 15,
+        margin: 5,
+        padding: 50,
+        width: '45%',
+        justifyContent: "space-between",
+        position: 'absolute',
+        bottom: 35,
+        left: 10
+    },
+
+    button2: {
+        flex: 1,
+        backgroundColor: '#B8D5B8',
+        borderRadius: 15,
+        margin: 5,
+        padding: 50,
+        width: "45%",
+        justifyContent: "space-between",
+        position: 'absolute',
+        bottom: 35,
+        left: 190
+    },
+
+    mt_100: {
+        marginTop:100
+    }
+
+
+});
+
+export {TouchView};
